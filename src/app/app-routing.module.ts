f5a8d02c0720330f8ff5cc92871cf0ca9dce4896
Atul Kamble <kambleatul26@gmail.com>
2019-09-11 12:53:06 +0530

    conns

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
+
+
+const routes: Routes = [
+  { path: '', component: ChatbotComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
